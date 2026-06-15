3fa0da43312dd244e68cc14515d61c6e8adc3ccb
Drone News <drone@news.local>
2026-06-15 15:54:10 +0800

    Update 2026-06-15 15:54

---
+++ docs/sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE = "drone-news-v1";
+const ASSETS = ["/drone-news/", "/drone-news/index.html", "/drone-news/manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (resp.ok) {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return resp;
+    }))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+});
